e263b3a455b0557b9f899ad89a5285ebb3a90108
maggie januarie <livjanua@gmail.com>
2024-07-24 16:51:44 +0200

    Added current time and a current weather condition img to our JS file, then improved our CSS and Html files

--- src/index.js
+++ src/index.js
@@ -11,6 +11,33 @@ function updateWeather(response) {
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   let windspeedElement = document.querySelector("#wind-speed");
   windspeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  let date = new Date(response.data.time * 1000);
+
+  let timeElement = document.querySelector("#time");
+  timeElement.innerHTML = formatDate(date);
+  let iconElement = document.querySelector("#icon");
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
